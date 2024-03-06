5403bc6bfe9601f93c0666ad8e62192ba641a128
Ankit Anshul <surenankit@gmail.com>
2024-03-06 20:12:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+const bar2 = document.getElementById("bar2");
+const nav2 = document.getElementById("navbar2");
+
+if (bar2) {
+  bar2.addEventListener("click", () => {
+    nav2.classList.add("active");
+  });
+}
+const bar3 = document.getElementById("bar3");
+const nav3 = document.getElementById("navbar3");
+
+if (bar3) {
+  bar3.addEventListener("click", () => {
+    nav3.classList.add("active");
+  });
+}
+const bar4 = document.getElementById("bar4");
+const nav4 = document.getElementById("navbar4");
+
+if (bar4) {
+  bar4.addEventListener("click", () => {
+    nav4.classList.add("active");
+  });
+}
+const bar5 = document.getElementById("bar5");
+const nav5 = document.getElementById("navbar5");
+
+if (bar5) {
+  bar5.addEventListener("click", () => {
+    nav5.classList.add("active");
+  });
+}
+const bar6 = document.getElementById("bar6");
+const nav6 = document.getElementById("navbar6");
+
+if (bar6) {
+  bar6.addEventListener("click", () => {
+    nav6.classList.add("active");
+  });
+}
+const bar7 = document.getElementById("bar7");
+const nav7 = document.getElementById("navbar7");
+
+if (bar7) {
+  bar7.addEventListener("click", () => {
+    nav7.classList.add("active");
+  });
+}
+const bar8 = document.getElementById("bar8");
+const nav8 = document.getElementById("navbar8");
+
+if (bar8) {
+  bar8.addEventListener("click", () => {
+    nav8.classList.add("active");
+  });
+}
+const bar9 = document.getElementById("bar9");
+const nav9 = document.getElementById("navbar9");
+
+if (bar9) {
+  bar9.addEventListener("click", () => {
+    nav9.classList.add("active");
+  });
+}
+const final = document.getElementById("final");
+if (final) {
+  final.addEventListener("click", () => {
+    alert("Congratulation, your purchase is done succesfully!");
+  });
+}
